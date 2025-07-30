2a0de5b913e7238c6360a6fa821c497d66c9891d
Sarthak160 <rocksarthak45@gmail.com>
2025-07-30 14:26:52 +0530

    bug

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'keploy_db',
         'USER': 'admin',
         'PASSWORD': 'keploy',
-        'HOST': 'mySQL',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
